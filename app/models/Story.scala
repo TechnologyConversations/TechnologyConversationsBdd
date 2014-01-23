51d0d455f25d631b590b696782b4352725c5c340
Viktor <trustno1>
2014-01-23 23:02:40 +0100

    Added retrieval of stories from FS

--- app/models/Story.scala
+++ app/models/Story.scala
@@ -1,10 +1,9 @@
 package models
 
-import anorm.SqlParser._
 import java.io.File
 
 case class Story(fileName: String) {
-  def name = fileName.split('.').init.mkString(".")
+  def name: String = fileName.split('.').init.mkString(".")
 }
 
 object Story {
@@ -12,13 +11,6 @@ object Story {
   def all(path: String = "stories"): List[Story] = {
     new File(path).list.filter(_.endsWith(".story")).map( file => Story(file)
     ).toList
-  }
-
-  // TODO Test
-  val story = {
-    get[String]("name") map {
-      name => Story(name)
-    }
   }
 
 }
--- test/models/StorySpec.scala
+++ test/models/StorySpec.scala
@@ -4,11 +4,19 @@ import org.specs2.mutable.Specification
 
 class StorySpec extends Specification {
 
-  "Story model" should {
+  "Story class" should {
+
+    "have name without extension" in {
+      Story("myStory.story").name must be matching "myStory"
+    }
+
+  }
+
+  "Story object" should {
 
     "return all stories from files ending with .story" in {
       val expected = (1 to 3).map(number => Story("story" + number + ".story"))
-      Story.all("test/data") should equalTo(expected)
+      Story.all("test/data") must equalTo(expected)
     }
 
   }
