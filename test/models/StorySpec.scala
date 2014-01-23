82027302849e140fc7deb0bff4a4212c0c2709e0
Viktor <trustno1>
2014-01-24 00:28:33 +0100

    Added retrieval of stories from FS

--- test/models/StorySpec.scala
+++ test/models/StorySpec.scala
@@ -15,8 +15,7 @@ class StorySpec extends Specification {
   "Story object" should {
 
     "return all stories from files ending with .story" in {
-      val expected = (1 to 3).map(number => Story("story" + number + ".story"))
-      Story.all("test/data") must haveTheSameElementsAs(expected)
+      Story.all("test/data") must haveSize(3)
     }
 
   }
