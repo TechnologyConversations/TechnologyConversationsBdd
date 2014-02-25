4c47c877b160aad396daf559edff58f38c3149fd
Viktor <trustno1>
2014-02-25 23:31:01 +0100

    Added open story menu item

--- public/html/index.js
+++ public/html/index.js
@@ -28,37 +28,48 @@ angular.module('storiesModule', ['ngRoute', 'ui.bootstrap', 'ui.sortable'])
             $modalInstance.close();
         };
     })
-    .controller('storiesCtrl', function($scope, $http, $modal) {
+    .controller('topMenuController', function($scope, $modal) {
+        $scope.openStory = function() {
+            $modal.open({
+                templateUrl: '/assets/html/stories.tmpl.html',
+                controller: 'storiesCtrl',
+                resolve: {
+                    data: function() {
+                        return {};
+                    }
+                }
+            });
+        };
+    })
+    .controller('storiesCtrl', function($scope, $http, $modal, $modalInstance) {
         $http.get('/stories/list.json').then(function(response) {
             $scope.files = response.data;
         }, function(response) {
             openModal($modal, response.data);
         });
+        $scope.ok = function () {
+            $modalInstance.close();
+        };
     })
     .controller('storyCtrl', function($scope, $http, $modal, story) {
-
         var originalStory = angular.copy(story);
         $scope.story = story;
         $scope.action = $scope.story.name === '' ? 'POST' : 'PUT';
-
         $scope.getCssClass = function(ngModelController) {
             return {
                 'has-error': ngModelController.$invalid,
                 'has-success': ngModelController.$valid && ngModelController.$dirty
             };
         };
-
         $scope.getButtonCssClass = function() {
             return {
                 'btn-success': $scope.storyForm.$valid,
                 'btn-danger': $scope.storyForm.$invalid
             };
         };
-
         $scope.canSaveStory = function() {
             return $scope.storyForm.$valid && !angular.equals($scope.story, originalStory);
         };
-
         $scope.saveStory = function() {
             if ($scope.canSaveStory()) {
                 if ($scope.action == 'POST') {
@@ -80,28 +91,22 @@ angular.module('storiesModule', ['ngRoute', 'ui.bootstrap', 'ui.sortable'])
                 }
             }
         };
-
         $scope.removeElement = function(collection, index) {
             collection.splice(index, 1);
         };
-
         $scope.addElement = function(collection, key) {
             collection.push({key: ''});
         };
-
         $scope.addScenarioElement = function(collection) {
             collection.push({title: '', meta: [], steps: [], examplesTable: ''});
         };
-
         $scope.revertStory = function() {
             $scope.story = angular.copy(originalStory);
             $scope.storyForm.$setPristine();
         };
-
         $scope.canRevertStory = function() {
             return !angular.equals($scope.story, originalStory);
         };
-
     });
 
 function getJson($http, $modal, url) {
