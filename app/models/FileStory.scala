dacb6148055374dd37d8262e22fda7b330ef6f2d
Viktor <trustno1>
2014-03-01 22:49:53 +0100

    Has option to create and delete directory

--- app/models/FileStory.scala
+++ app/models/FileStory.scala
@@ -38,7 +38,7 @@ trait FileStory {
       if (file.isFile) {
         file.delete
       } else {
-        val filePath = Path(path)
+        val filePath = Path.fromString(path)
         val (deleted, remaining) = filePath.deleteRecursively()
         remaining == 0
       }
