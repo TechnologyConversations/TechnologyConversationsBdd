ac6c72535b36af5ac22e67d14de396c9757478df
Viktor <trustno1>
2014-03-04 23:28:17 +0100

    Finished JBehaveSteps

--- app/models/JBehaveSteps.scala
+++ app/models/JBehaveSteps.scala
@@ -2,8 +2,17 @@ package models
 
 import java.io.File
 import org.clapper.classutil.ClassFinder
+import play.api.libs.json.{Json, JsValue}
+import org.jbehave.core.steps.{StepCandidate, Steps}
+import org.jbehave.core.configuration.MostUsefulConfiguration
+import scala.collection.JavaConversions._
 
 class JBehaveSteps(dir: String = "steps") {
+
+  def toJson: JsValue = {
+    val steps = stepsCandidates.map(step => Map("step" -> step.toString))
+    Json.toJson(Map("steps" -> Json.toJson(steps)))
+  }
 
   private[models] def stepsJars = {
     new File(dir).listFiles.filter(_.getName.endsWith(".jar")).toList
@@ -11,6 +20,24 @@ class JBehaveSteps(dir: String = "steps") {
 
   private[models] def stepsClasses = {
     ClassFinder(stepsJars).getClasses().filter(_.isConcrete).toList
+  }
+
+  private[models] def steps = {
+    val config = new MostUsefulConfiguration()
+    stepsClasses.map { stepsClass =>
+      new Steps(config, Class.forName(stepsClass.name).newInstance())
+    }
+  }
+
+  private[models] def stepsCandidates = {
+    def stepsCandidates(steps: List[Steps], candidates: List[StepCandidate]): List[StepCandidate] = {
+      if (steps.isEmpty) {
+        candidates
+      } else {
+        stepsCandidates(steps.tail, candidates ::: steps.head.listCandidates().toList)
+      }
+    }
+    stepsCandidates(steps, List())
   }
 
 }
--- test/models/JBehaveStepsSpec.scala
+++ test/models/JBehaveStepsSpec.scala
@@ -3,15 +3,19 @@ package models
 import org.specs2.mutable.Specification
 import java.io.File
 import org.clapper.classutil.ClassInfo
+import play.api.libs.json.{JsValue, Json}
+import org.jbehave.core.steps.{StepCandidate, Steps}
+import org.specs2.matcher.JsonMatchers
 
-class JBehaveStepsSpec extends Specification {
+class JBehaveStepsSpec extends Specification with JsonMatchers {
 
   "JBehaveSteps#stepsJars" should {
 
     "return a list of all JARs" in {
-      val jars = JBehaveSteps().stepsJars
-      jars must beAnInstanceOf[List[File]]
-      jars.size must beGreaterThan(0)
+      val list = JBehaveSteps().stepsJars
+      list must beAnInstanceOf[List[File]]
+      list.size must beGreaterThan(0)
+      list(0) must beAnInstanceOf[File]
     }
 
   }
@@ -19,12 +23,51 @@ class JBehaveStepsSpec extends Specification {
   "JBehaveSteps#stepsClasses" should {
 
     "return a list of all classes" in {
-      val classes = JBehaveSteps().stepsClasses
-      classes must beAnInstanceOf[List[ClassInfo]]
-//      Class.forName(classes(0).name).newInstance()
-      classes.size must beGreaterThan(0)
+      val list = JBehaveSteps().stepsClasses
+      list must beAnInstanceOf[List[ClassInfo]]
+      list.size must beGreaterThan(0)
+      list(0) must beAnInstanceOf[ClassInfo]
     }
 
   }
 
+  "JBehaveSteps#steps" should {
+
+    "return a list of all steps" in {
+      val list = JBehaveSteps().steps
+      list must beAnInstanceOf[List[Steps]]
+      list.size must beGreaterThan(0)
+      list(0) must beAnInstanceOf[Steps]
+    }
+
+  }
+
+  "JBehaveSteps#stepsCandidates" should {
+
+    "return a list of all steps candidates" in {
+      val list = JBehaveSteps().stepsCandidates
+      list must beAnInstanceOf[List[Steps]]
+      list.size must beGreaterThan(0)
+      list(0) must beAnInstanceOf[StepCandidate]
+    }
+
+  }
+
+
+  "JBehaveSteps#toJson" should {
+
+    val json = JBehaveSteps().toJson
+    val jsonString = json.toString()
+
+    "return JSON" in {
+      json must beAnInstanceOf[JsValue]
+    }
+
+    "return JSON with the correct format" in {
+      jsonString must /("steps") */("step" -> "GIVEN .*".r)
+      jsonString must /("steps") */("step" -> "WHEN .*".r)
+      jsonString must /("steps") */("step" -> "THEN .*".r)
+    }
+
+  }
 }
