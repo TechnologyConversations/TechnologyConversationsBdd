cecb7e40efa8f8c8003ae3e614e76951d26f3646
Viktor <trustno1>
2014-03-28 09:19:03 +0100

    Added JBehaveComposites#toJson

---
+++ app/composites/com/technologyconversations/bdd/steps/WebStepsComposites.java
@@ -0,0 +1,15 @@
+package composites.com.technologyconversations.bdd.steps;
+
+import org.jbehave.core.annotations.*;
+
+public class WebStepsComposites {
+
+    @Given("this is my composite")
+    @Composite(steps = {"Given something", "When else", "Then OK"})
+    public void compositeStep0() { }
+
+    @Given("this is \"my\" <param1>, <param2> and <param3>")
+    @Composite(steps = {"Given \"my\" <param1>", "When <param2>", "Then <param3>"})
+    public void compositeStep1(@Named("param1") String param1, @Named("param2") String param2, @Named("param3") String param3) { }
+
+}
