304f1b990b09c69cbb68ba4014e34bfbd919b284
Viktor Farcic <viktor.farcic@everis.com>
2014-08-29 12:47:18 +0200

    Moved compositeClassesModule to compositesModule

--- public/html/composites/composites.ctrl.js
+++ public/html/composites/composites.ctrl.js
@@ -111,4 +111,50 @@ angular.module('compositesModule', [])
                 return 'Update Composites';
             }
         };
+    })
+    .controller('compositeClassesCtrl', function($scope, $http, $modalInstance, compositeClasses, compositeStepText, TcBddService) {
+        $scope.compositeClasses = compositeClasses;
+        $scope.compositeStepText = compositeStepText;
+        $scope.close = function() {
+            $modalInstance.close();
+        };
+        $scope.compositeClassUrl = function(packageName, className) {
+            var url = '/page/composites/';
+            if (packageName !== undefined && packageName !== '') {
+                url += packageName + '.';
+            }
+            var classNameParts;
+            if (className.indexOf('/') >= 0) {
+                classNameParts = className.split('/');
+            } else {
+                classNameParts = className.split('\\');
+            }
+            url += classNameParts[classNameParts.length - 1];
+            if ($scope.compositeStepText !== undefined && $scope.compositeStepText !== '') {
+                url += '?stepText=' + $scope.compositeStepText;
+            }
+            return url;
+        };
+        $scope.compositeClassText = function(className) {
+            var classNameParts;
+            if (className.indexOf('/') >= 0) {
+                classNameParts = className.split('/');
+            } else {
+                classNameParts = className.split('\\');
+            }
+            var text = classNameParts[classNameParts.length - 1];
+            if (text.indexOf('.') >= 0) {
+                text = text.substr(0, text.lastIndexOf('.'));
+            }
+            return text;
+        };
+        $scope.classNamePattern = TcBddService.classNamePattern;
+        $scope.cssClass = TcBddService.cssClass;
+        $scope.data = {class: ''};
+        $scope.onFinishJoyRide = function() {
+            TcBddService.onFinishJoyRide($scope);
+        };
+        $scope.startJoyRide = function(id) {
+            TcBddService.startJoyRide(id, $scope);
+        };
     });
