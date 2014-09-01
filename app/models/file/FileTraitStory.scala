f79032434e12345bbc77171fa6ff3a19ab354a5d
Viktor Farcic <viktor.farcic@everis.com>
2014-09-01 13:18:30 +0200

    Deprecations

--- app/models/file/FileTraitStory.scala
+++ app/models/file/FileTraitStory.scala
@@ -2,6 +2,7 @@ package models.file
 
 import java.io.File
 
+@deprecated("Use BddFile instead")
 trait FileTraitStory extends BddFileTrait {
 
   def name: String = new File(fullPath).getName.split('.').init.mkString(".")
