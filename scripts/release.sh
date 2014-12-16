2886d0c2345ca034e8c5561737470e0d62d3084c
vifarcic <viktor.farcic@everis.com>
2014-12-16 18:55:29 +0100

    Added creation of the distribution directory directory placeholder

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir -p release/bdd-assistant
-cp -R composites conf data LICENCE.txt public steps target release//bdd-assistant/.
+mkdir -p release/bdd-assistant/build/reports/tests
+cp -R composites conf data LICENCE.txt public steps target release/bdd-assistant/.
 cd release
 rm -rf release/bdd-assistant
 tar -czf relese.tar.gz bdd-assistant
